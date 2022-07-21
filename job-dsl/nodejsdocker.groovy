184b1b1c1a3a76093c54f85357c4eed6761fe54c
pegasus120mouan <106601751+pegasus120mouan@users.noreply.github.com>
2022-07-21 20:31:44 +0000

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/pegasus120mouan/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('pegasus120mouan')
+            node / gitConfigEmail('coulibaly.fisher@yahoo.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('pegasus120mouan/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
